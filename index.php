389a46354df0586e3adb8834d7d04ed233357cf8
Paulo Soares <paulosoares754@gmail.com>
2023-03-28 22:36:20 -0300

    resolvi o conflito de olas

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 
-<<<<<<< HEAD
 echo "Olá Mundo 256!";
 
-echo "Olá Udemy!";
-=======
-echo "Olá Mundo 123!";
+echo "Olá Udemy e paulo!";
 
-echo "Olá paulo!";
->>>>>>> b68489268ebb6b2ed516fc792c0e6a85a98b9630
 
 ?>
